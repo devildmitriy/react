7e0e7f106262ee49785629c1d391576d2b953feb
devildmitriy <kozlovdmitriy@mail.ru>
2022-05-06 16:51:00 +0600

    finish section15 - create custom hook

---
+++ section15_vite/15/src/hooks/use-counter.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const useCounter = (forwards) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => {
+        if (forwards) {
+         return prevCounter + 1;
+        } else {
+           return prevCounter - 1
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
+
+export default useCounter;
---
+++ section15_vite/http-hooks/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    sendRequest(
+      {
+        url: "https://react-http-3bdcd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+      },
+      transformTasks
+    );
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={sendRequest}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
